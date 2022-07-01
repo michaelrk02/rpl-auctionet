606f5cee8d08ebbe6f4418f57ac5202176745e14
klaudiusandhika <klaudiusandhika@gmail.com>
2022-07-01 22:16:13 +0700

    view pengiriman

--- resources/views/bidder/pengiriman/semua.blade.php
+++ resources/views/bidder/pengiriman/semua.blade.php
@@ -4,42 +4,30 @@ use App\Libraries\Auctionet;
 
 @extends('layouts.bidder')
 
-@section('title', 'Balance Management')
+@section('title', 'Balance')
 
 @section('content')
 
 {{-- 
-// Tampilkan tabel yang berisi:
+  // Tampilkan tabel yang berisi:
     // - Waktu
     // - Produk yg dikirim (sertakan link ke route detail produknya)
     // - Layanan
     // - No. Resi
     // - Alamat --}}
-<body>
+
         <div class="container">
-                <div class="title d-flex justify-content-center align-items-center mb-4">
+                <div class="title d-flex justify-content-center align-items-center mb-4 ">
                     <img src="/assets/img/auc-logo.png" alt="" style="height: 30px">
                 </div>
 
-                <div class="balance-main d-flex justify-content-between align-items-center">
-                    <div class="main-1" style="width: 250px">
-                        <h5 class="text-light mb-2 fw-bold">Your Current Balance</h5>
-                        <h4 class="text-light mb-2" style="font-size: 42px">IDR. 500.000</h4>
+                <div class="shipment-main d-flex justify-content-start align-items-center">
+                    <div class="main-1">
+                        <i class="bi bi-geo-alt text-light" style="font-size:50px;"></i>
                     </div>
-                    <div class="main-2 d-flex justify-content-around align-items-end"
-                    style="width: 160px; height:100px; ">
-                        <div class="topup text-center">
-                            <button type="submit" onclick="location.href='{{ route('bidder.saldo.topup') }}';" class="btn btn-topup btn-primary fw-bold" style="height: 42px;">
-                                <i class="bi bi-plus-lg topup-icon" style="font-size: 20px;"></i>
-                            </button>
-                            <p class="text-light">Top Up</p>
-                        </div>
-                        <div class="tarik text-center">
-                            <button type="submit" onclick="location.href='{{ route('bidder.saldo.tarik') }}';" class="btn btn-tarik btn-primary fw-bold" style="height: 42px">
-                                <i class="bi bi-arrow-down-up" style="font-size: 20px;"></i>
-                            </button>
-                            <p class="text-light">Withdraw</p>
-                        </div>
+                    <div class="main-2 d-flex justify-content-start align-items-center mt-2"
+                    style=" height:50px; margin-left:10px">
+                        <h2 class="text-light">Track Your Items!</h2>
                     </div>  
                 </div>
                 <div class="table-wrapper-scroll-y my-custom-scrollbar mt-3">
@@ -47,31 +35,24 @@ use App\Libraries\Auctionet;
                         <thead class="">
                             <tr>
                                 <th scope="col">Time</th>
-                                <th scope="col">Type</th>
-                                <th scope="col">Amount</th>
-                                <th scope="col">Information</th>
+                                <th scope="col">Item</th>
+                                <th scope="col">Service</th>
+                                <th scope="col">Receipt</th>
+                                <th scope="col">Destination</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td>-</td>
-                                <td>Withdrawal</td>
-                                <td>-</td>
-                                <td>Successful</td>
+                                <td>05.00</td>
+                                <td><a href=""
+                                    class=" text-decoration-none fw-bold">Sendok Sup Abad 17</a></td>
+                                <td>REG</td>
+                                <td>JT-8734802832345</td>
+                                <td>Banjarsari, SOC</td>
+
                             </tr>
                             <tr>
                                 <td>-</td>
-                                <td>-</td>
-                                <td>-</td>
-                                <td>-</td>
-                            </tr>
-                            <tr>
-                                <td>-</td>
-                                <td>-</td>
-                                <td>-</td>
-                                <td>-</td>
-                            </tr>
-                            <tr>
                                 <td>-</td>
                                 <td>-</td>
                                 <td>-</td>
@@ -82,8 +63,24 @@ use App\Libraries\Auctionet;
                                 <td>-</td>
                                 <td>-</td>
                                 <td>-</td>
+                                <td>-</td>
                             </tr>
                             <tr>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                            </tr>
+                            <tr>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                            </tr>
+                            <tr>
+                                <td>-</td>
                                 <td>-</td>
                                 <td>-</td>
                                 <td>-</td>
@@ -93,9 +90,7 @@ use App\Libraries\Auctionet;
                     </table>
                 </div>
             </div>
-	
-</body>
-@endsection	
+@endsection		
 
 @push('styles')
 <style>
@@ -108,7 +103,7 @@ use App\Libraries\Auctionet;
     box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.5);
 }
 
-.balance-main{
+.shipment-main{
     background-color:#00000094;
     border: solid 2px rgba(197, 197, 197, 0.623);
     border-radius: 10px;
